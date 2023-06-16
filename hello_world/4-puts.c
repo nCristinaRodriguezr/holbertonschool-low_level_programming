c363beb89cec2ce85a5c3693d1d2b2d6c3828429
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-16 16:23:42 -0700

    punto 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main() {
-           /* Imprime la cadena Programming is like building a multilingual puzzle */    
-           puts("Programming is like building a multilingual puzzle");
-           return 0;
-           }
+/*
+ * Imprime la cadena Programming is like
+ * building a multilingual puzzle
+ */
+int main(void)
+	{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+	}
