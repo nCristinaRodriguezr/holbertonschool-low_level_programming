f01123beff47d92c766d6b649235578fcbbd57b2
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-16 16:43:02 -0700

    punto 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  Imprime la cadena Programming is like
+ * building a multilingual puzzle
+ * Return: siempre devuelve 0
+ */
+int main(void)
+	{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+	}
