af0938eb4d284ab9237b3e5faff0274739bf2889
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-21 20:49:10 -0700

    alfabeto ahora en MAYUSCULA

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- imprimirá el alfabeto en minuscula y MAYUSCULA
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'a' - ('a' - 'A'); c <= 'z' - ('a' - 'A'); c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
