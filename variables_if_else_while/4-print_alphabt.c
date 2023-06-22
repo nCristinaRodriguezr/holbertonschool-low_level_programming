38a294f66f93f0ec55edf79ee4594d28d8883830
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-21 21:00:19 -0700

    alfabeto excluyendo letras

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,16 @@
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- imprimirá el alfabeto en minuscula excluyendo letras
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'q' && c != 'e')
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
