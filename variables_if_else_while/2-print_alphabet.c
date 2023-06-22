243ac7cb4b4115d452c6e99ae8282839a3686efc
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-21 20:25:45 -0700

    alfabeto

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- imprimirá el alfabeto en minuscula
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+	return (0);
+}
