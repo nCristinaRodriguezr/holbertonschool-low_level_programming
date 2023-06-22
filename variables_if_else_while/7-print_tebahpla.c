e771cb799bfe5f96975610bbf32a1306647bd443
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-22 08:30:32 -0700

    alfabeto al revés

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- imprimirá el alfabeto en minuscula al revés
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
