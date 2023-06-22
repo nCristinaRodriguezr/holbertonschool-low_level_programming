097fde83ab199651dac5f11f400beab13fa4c226
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-21 21:18:16 -0700

    numeros de un solo digito en base 10

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- imprimira numeros de un solo digito en base 10
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
