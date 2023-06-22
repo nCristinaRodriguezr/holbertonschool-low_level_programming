6b9df5c93e304e8143db9eca141f3e495a46c497
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-22 12:53:32 -0700

    numeritos comita espacio

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- imprimirá todas las combinaciones
+ * posibles de números de un solo dígito
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
