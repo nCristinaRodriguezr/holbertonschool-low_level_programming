1aac3ae9e2178cf75dc49c4991e199fb68895631
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-23 14:23:10 -0700

    no seee

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- imprimirá _putchar
+ * Return: siempre devuelve 0
+ */
+void print_alphabet(void)
+{
+	char i;
+	    for (i = 'a'; i <= 'z'; i++)
+		            putchar(i);
+}
