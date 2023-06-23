e328cbe10ab713919261fa0d73f85e935133a91d
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-22 18:14:46 -0700

    puchi puchi

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- imprimirá _putchar
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	char c[] = "_putchar\n";
+	int i = 0;
+		while (c[i] != '\0')
+		{
+			write(1, &c[i], 1);
+			i++;
+		}
+	return (0);
+}
