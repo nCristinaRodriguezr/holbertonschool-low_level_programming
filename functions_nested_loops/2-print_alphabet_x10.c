49d92b3f1528c2d8ddd2820f4f13df68f69c6fa2
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-23 15:59:59 -0700

    alfabeto x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet- imprimirá el alfabeto en minuscula
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
