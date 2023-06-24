51424e23ee0db048e450de0162859599d446b6bf
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-24 16:33:54 -0700

    No me gustó la banda sonora sugerida

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * jack_bauer- imrpime el último dígito de un número
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
+		}
+	}
+}
