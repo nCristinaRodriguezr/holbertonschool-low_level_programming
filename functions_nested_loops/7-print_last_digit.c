ac3a2b9d702602acf8f7155eba7aa81ee1b4e8a5
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-24 12:03:56 -0700

    ultimo numero

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_last_digit- imrpime el último dígito de un número
+ * @n: variable de entrada
+ * Return: el valor absoluto
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	return (last_digit);
+}
