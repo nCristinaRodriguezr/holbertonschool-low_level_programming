4464996968d294a11e22d8a071ee6a9416ab097c
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-23 21:15:12 -0700

    devuelve 0,1 o -1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,19 +12,21 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		int c = '+';
+
 		write(1, &c, 1);
 		return (1);
 	}
 	else if (n == 0)
 	{
 		int c = '0';
+
 		write(1, &c, 1);
 		return (0);
 	}
-	
 	else
 	{
 		int c = '-';
+
 		write(1, &c, 1);
 		return (-1);
 	}
