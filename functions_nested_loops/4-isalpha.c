43e29a97f9452c8e48806df0aaa9e9c9331a3d53
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-23 17:09:05 -0700

    devuelve 1 si es leta 0 si no

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha- devuelve 1 si es letra y 0 si no
+ * @c: variable de entrada
+ * Return: devuelve 0 o 1
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c) > 0)
+		return (1);
+	else
+		return (0);
+}
