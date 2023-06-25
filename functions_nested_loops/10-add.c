b5b6890003b6a1b74b14d5a1b0cd97a277255cc1
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-24 18:34:18 -0700

    sumita

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <unistd.h>
+/**
+ * add- suma suma suma
+ * @n1: variable 1
+ * @n2: variable 2
+ * Return: devuelve resultado
+ */
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+		return (sum);
+}
