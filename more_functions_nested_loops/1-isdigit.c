e13c65f0fd60e8e2f3ab6fec87047e922cfb95ea
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-25 16:24:18 -0700

    digiturno

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-#include <ctype.h>
-#include <unistd.h>
 /**
- *  _isupper-verifica el carácter en mayúsculas
+ * _isdigit-verifica un digito de 0 a 9
  * @c: variable de entrada
  * Return: devuelve 0 o 1
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
