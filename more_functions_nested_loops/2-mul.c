c5cc27c8e9b28c35c07e06d284198a9c14b2e061
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-25 16:33:42 -0700

    numerito

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul- multiplica 2 enteros
+ * @a: variable de entrada
+ * @b: otra variable
+ * Return: resultado
+ */
+int mul(int a, int b)
+{
+	int result = (a * b);
+		return (result);
+}
