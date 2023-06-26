5e13f09d53e9986f164c5d7c2016a02183d2dbae
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-25 18:07:14 -0700

    linea recta

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_line- imprimirá una linea recta
+ * @n: variable 1
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
