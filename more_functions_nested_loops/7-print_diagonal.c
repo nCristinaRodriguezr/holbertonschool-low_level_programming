dab9b5ea739fc236641ad9a38475e9c9bf757e4d
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-25 18:27:22 -0700

    callejon diagon

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,16 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		putchar('\n');
 		return;
 	}
-	int i;
-
 	for (i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
 			putchar(' ');
 		}
