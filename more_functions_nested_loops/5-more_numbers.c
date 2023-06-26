79e847b1d6cba32f5b9392352ec236e9bcbea796
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-25 17:21:48 -0700

    numeritos repetidos x10

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,10 +6,12 @@
  */
 void more_numbers(void)
 {
-	 int i, j;
+	 int i;
 
 	for (i = 0; i < 10; i++)
 	{
+		int j;
+
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
@@ -18,6 +20,6 @@ void more_numbers(void)
 				_putchar((j % 10) + '0');
 			}
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
