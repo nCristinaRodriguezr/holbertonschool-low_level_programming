60552b14b9a2a2573596353fc761866ff7ba66dc
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-25 17:31:55 -0700

    numeritos repetidos x10

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,24 +2,22 @@
 #include "main.h"
 /**
  * more_numbers-imprime 10 veces los numeros
- * del 1 al 14
+ * del 0 al 14
  */
 void more_numbers(void)
 {
-	 int i;
+	 int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		int j;
-
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
+			_putchar((j % 10) + '0');
 		}
+		 _putchar('\n');
 	}
-	_putchar('\n');
 }
