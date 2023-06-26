6b41ebd919bf1c1e0b14a41a47137d44238a6f9e
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-25 20:31:42 -0700

    Fizz Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number-imprime numero dado
+ * @n: variable 1
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		print_number(n / 10);
+		n %= 10;
+	}
+	_putchar('0' + n);
+}
+/**
+ * print_text-imprime cadena de caracteres
+ * @c: variable 1
+ */
+void print_text(const char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+}
+/**
+ * fizzBuzz-imprima los números del 1 al 100
+ * Pero para los múltiplos de tres, escriba Fizz en lugar del número
+ * para los múltiplos de cinco, escriba Buzz
+ * Para números que son múltiplos de tres y cinco, imprima FizzBuzz
+ */
+void fizzBuzz(void)
+{
+	int i, j;
+
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char fizzBuzz[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			print_text(fizzBuzz);
+		}
+		else if (i % 3 == 0)
+		{
+			print_text(fizz);
+		}
+		else if (i % 5 == 0)
+		{
+			print_text(buzz);
+		}
+		else
+		{
+			print_number(i);
+		}
+	}
+	putchar('\n');
+}
+/**
+ * main- imprimirá _putchar
+ * Return: siempre devuelve 0
+ */
+int main(void)
+{
+	fizzBuzz();
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -8,3 +8,6 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_number(int n);
+void fizzBuzz(void);
+void print_text(const char *c);
