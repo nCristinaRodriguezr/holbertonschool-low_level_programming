2c06afec558d28ef393f94c9b799a17c31634654
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-27 07:46:51 -0700

    punterito

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98- actualiza el valor al que apunta a 98
+ * @n: variable 1
+ */
+void reset_to_98(int *n)
+{
+	*n = (98);
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Punteros y otras cositas
