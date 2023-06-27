af13492fbc3c848eae2ba3ca3061a3de9422804a
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-27 13:40:41 -0700

    anedac

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_rev-imprime una cadena, al revés
+ * @s: variable de entrada
+ */
+void print_rev(char *s)
+{
+	int length = strlen(s);
+
+	for (int i = length - 1; i >= 0; i--)
+	{
+		write(1, &s[i], 1);
+	}
+	write(1, "\n", 1);
+}
