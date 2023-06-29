0ba303e5fbb09f961cee262a1e887a17128d6db0
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-29 15:46:01 -0700

    prints n elements of an array of integers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_array-imprime n elementos de un array de enteros
+ * @a: variable de entrada
+ * @n: variable 2
+ */
+void print_array(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+}
