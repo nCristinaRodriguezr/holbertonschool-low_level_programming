92301b87ae92a1badcf1ff0c6970c07784fc25e1
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-29 07:59:02 -0700

    every other

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2-imprime una caracter si y otro no de una cadena de texto
+ * @str: variable de entrada
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i = i + 2;
+	}
+}
