4132ba011bd48c14b2abdefd735725f6693b08fe
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-30 13:55:37 -0700

    converts a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
+	int sign = -1;
 	int result = 0;
 
 	while (*s != '\0' && (*s < '0' || *s > '9'))
@@ -19,7 +19,7 @@ int _atoi(char *s)
 	}
 	while (*s >= '0' && *s <= '9')
 	{
-		result = result * 10 + (*s - '0');
+		result = result * 10 - (*s - '0');
 		s++;
 	}
 	return (result * sign);
