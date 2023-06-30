dd1528ccabc0145175554073f1ebed47029c9f89
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-06-30 10:54:10 -0700

    converts a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,10 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 
-	while (*s == ' ' || *s == '\t' || *s == '\n')
-		s++;
-
-	if (*s == '-')
+	while (*s != '\0' && (*s < '0' || *s > '9'))
 	{
-		sign = -1;
-		s++;
-	}
-	else if (*s == '+')
-	{
+		if (*s == '-')
+			sign *= -1;
 		s++;
 	}
 	while (*s >= '0' && *s <= '9')
