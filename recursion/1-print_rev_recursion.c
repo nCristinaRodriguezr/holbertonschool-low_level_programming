99ae5fd34a63ecb6e70f7f6f04e83776dc5168a5
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-05 09:50:45 -0700

    prints a string in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion- imprime una cadena al revés
+ * seguida de una nueva línea.
+ * @s: variable 1
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+		putchar(*s);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
