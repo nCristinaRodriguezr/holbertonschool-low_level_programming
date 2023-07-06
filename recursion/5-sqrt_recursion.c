c3887f089423c4e906809a9a0ec7b44cfdd25cbd
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-06 16:41:51 -0700

    fix to one checker

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,26 +7,26 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (recursion_max(n, n));
+	return (recursion_max(n, 0));
 }
 /**
  * recursion_max- devuelva la raíz cuadrada natural de un número.
  * @n: Numero entregado por el usuario para sacar la raiz cuadrada
- * @i: Guarda n para no perder la referencia del numero original
+ * @i: es la variable que se mueve de 0 al numero original o del usuario
  * Return: devuelve el resultado de la raiz cuadra de n
  */
 int recursion_max(int n, int i)
 {
-	if (n < 0)
+	if (i > n)
 	{
 		return (-1);
 	}
-	else if (i == 0 || i == 1 || n * n == i)
+	else if (i * i == n)
 	{
-		return (n);
+		return (i);
 	}
 	else
 	{
-		return (recursion_max(n - 1, i));
+		return (recursion_max(n, i++));
 	}
 }
