ffac9b2eadabecd2b562b2ef4ef0fd7fdcd9027e
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-08 16:26:25 -0700

    static librari script

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,35 +1,15 @@
- # CC: Es el compilador que vamos a usar
+#!/bin/bash
 
- # CFLAGS:son las opciones de compilación, en este caso,
- # solo -c para generar archivos objeto.
+# Recopila los nombres de los archivos .c en una variable
+c_files=$(ls *.c)
 
- # AR: es el comando para crear archivos de biblioteca estática.
+# Compila los archivos .c en archivos objeto .o
+for file in $c_files; do
+	    gcc -c $file
+    done
 
- # ARFLAGS: son las opciones para el comando ar, en este caso,
- # rcs para crear una biblioteca estática.
+    # Crea la biblioteca estática liball.a
+    ar rcs liball.a *.o
 
- # SRCS: es una variable que utiliza el comando wildcard para
- # buscar todos los archivos .c en el directorio actual.
-
- # OBJS es una variable que reemplaza la extensión .c por .o
- # $@ $^: son variables que se usan para referirse a el nombre
- #  y las lista de los archivos .o
-
-CC := gcc
-CFLAGS := -c
-AR := ar
-ARFLAGS := rcs
-
-SRCS := $(wildcard *.c)
-OBJS := $(SRCS:.c=.o)
-
-all: liball.a
-
-liball.a: $(OBJS)
-	$(AR) $(ARFLAGS) $@ $^
-
-%.o: %.c
-	$(CC) $(CFLAGS) -o $@ $<
-
-clean:
-	rm -f liball.a $(OBJS)
+    # Limpia los archivos objeto generados
+    rm -f *.o
