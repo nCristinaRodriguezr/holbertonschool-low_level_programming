6f4dd290e29d4cf69a387a3aa465df5520a95246
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-08 19:00:01 -0700

    print the arguments number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- imprimirá el numero de argumentos
+ * @argc: es un contador de argumentos
+ * @argv: es el apuntador a una matriz
+ * Return: Devuelve 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
