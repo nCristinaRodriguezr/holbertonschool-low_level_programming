314710fc728b8aa1525c862977fd0e1f2b78c249
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-13 13:44:47 -0700

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,19 +14,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *result;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	if (n >= strlen(s2))
 	{
 		n = strlen(s2);
 	}
-	total_len = strlen(s1) + n;
-	result = (char *)malloc(total_len);
+	total_len = strlen(s1) + n + 1;
+	result = (char *)malloc(total_len * sizeof(char));
 	if (result == NULL)
 	{
 		return (NULL);
