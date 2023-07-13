ead80ce839ab3d6c822b3a457dcc2b516f269cb2
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-13 06:55:03 -0700

    fix of freedom

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,6 +21,7 @@ int **alloc_grid(int width, int height)
 	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
+		free_grid2(grid, height);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -28,15 +29,10 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *)calloc(width, sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
-			{
-				free(grid[j]);
-			}
-			free(grid);
+			free_grid2(grid, height);
 			return (NULL);
 		}
 	}
-	free_grid2(grid, height);
 	return (grid);
 }
 /**
