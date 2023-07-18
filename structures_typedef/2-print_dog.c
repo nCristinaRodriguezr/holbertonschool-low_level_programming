048ea14345d4d854b4d34a1d7f338136bd357e59
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-18 14:51:51 -0700

    :dog3: struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog- imprime la estructura dog
+ * @d: puntero
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Nombre: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Edad: %.2f\n", d->age);
+	printf("Dueño: %s\n", d->owner != NULL ? d->owner : "(nil)");
+}
