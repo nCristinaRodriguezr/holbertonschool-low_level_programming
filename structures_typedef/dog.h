f80a1792e7247c7ae830ae2f52b7c661972dccd1
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-18 13:44:54 -0700

    :dog: struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- estructura que representa un perrito
+ * @name: nombre del perrito
+ * @age: edad del perrito
+ * @owner: dueño del perrito
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
