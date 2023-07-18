7c2d371b9614b73316d5fe3919a00f4695d32a8f
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-18 15:19:34 -0700

    :rocket: struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,19 +11,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * struct dog_t- estructura que representa un perrito
- * @name: nombre del perrito
- * @age: edad del perrito
- * @owner: dueño del perrito
- */
-struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-};
 #endif
