7eec32de54c704f95f286181e2543629b772642c
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-23 16:48:34 -0700

    calc

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,5 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
+int main(int argc, char *argv[]);
 /**
  * struct op - Struct op
  * @op: The operator
@@ -16,5 +17,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-int main(int argc, char *argv[]);
 #endif
