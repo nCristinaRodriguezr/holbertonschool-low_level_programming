deae4cd9c5703e8da0f1057ebe5e5d98e41e3c73
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-23 19:13:30 -0700

    print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings-imprime una cadena
+ * @separator: Puntero
+ * @n: argumento constante y sin signo
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+	for (unsigned int i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
