4aa1c5d56c45272556f6917dab51379402577a25
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-07-23 17:32:38 -0700

    sum

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,6 +10,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	va_list args;
+	unsigned int i;
 
 	va_start(args, n);
 
@@ -17,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
