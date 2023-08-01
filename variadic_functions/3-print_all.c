e27ec57775e9c76b4093732d630689879fd9fb8d
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-01 14:42:04 -0700

    task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all- imprime todo
+ * @format: puntero que la cadena que el usuario desea imprimir
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *esp = "", *coma = ", ";
+	va_list argts;
+
+	type_var variables[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_str},
+		{NULL, NULL}
+	};
+	va_start(argts, format);
+	while (format && format[i])
+	{
+		x = 0;
+		while (x < 4)
+		{
+			if (format[i] == *(variables[x].ptr))
+			{
+				printf("%s", esp);
+				variables[x].function(argts);
+				esp = coma;
+			}
+			x++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(argts);
+}
+/**
+ * p_char - print char
+ * @argts: list of arguments
+ */
+void p_char(va_list argts)
+{
+	printf("%c", va_arg(argts, int));
+}
+/**
+ * p_int - print int
+ * @argts: list of arguments
+ */
+void p_int(va_list argts)
+{
+	printf("%d", va_arg(argts, int));
+}
+/**
+ * p_float - print float
+ * @argts: list of arguments
+ */
+void p_float(va_list argts)
+{
+	printf("%f", va_arg(argts, double));
+}
+/**
+ * p_str - print string
+ * @argts: list of arguments
+ */
+void p_str(va_list argts)
+{
+	char *str = va_arg(argts, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,4 +3,14 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct variables - Struct functions
+ * @ptr: Apunta al tipo de dato o de especificador
+ * @function: Funcion asociada
+ */
+typedef struct variables
+{
+	char *ptr;
+	void (*function)(va_list argts);
+} type_var;
 #endif
