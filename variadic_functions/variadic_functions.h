bef2980c4c012c3d81a250c0887af258432cd416
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-01 14:48:18 -0700

    task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,6 +3,10 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void datac(va_list argts);
+void datanum(va_list argts);
+void  dataf(va_list argts);
+void datas(va_list argts);
 /**
  * struct variables - Struct functions
  * @ptr: Apunta al tipo de dato o de especificador
