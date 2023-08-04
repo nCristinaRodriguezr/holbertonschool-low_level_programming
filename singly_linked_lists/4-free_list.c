a0969b74d280e9c1bd5161351f03876a1be42e52
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-03 21:00:04 -0700

    trying task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * free_list- Funcion que libero una lista
+ * @head: A la lista que el usuario desea liberar
+ */
+void free_list(list_t *head)
+{
+	list_t *h = head;
+	list_t *next;
+
+	while (h != NULL)
+	{
+		next = h->next;
+		free(h);
+		h = next;
+	}
+}
+
