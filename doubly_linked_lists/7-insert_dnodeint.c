ac66956cbd4ab6737895184dc7099e4111d8da07
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-06 19:33:05 -0700

    task 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index- Funcion que añade un nuevo nodo
+ * en la posicion dada.
+ * @h: A la lista de nodos
+ * @idx: indice o posicion dada donde se debe insertar el nuevo nodo
+ * @n: valor que hay dentro del nodo
+ * Return: Devuelve el nuevo nodo
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	if (idx == 0 || *h == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
+	}
+	dlistint_t *current = *h;
+	unsigned int count = 0;
+
+	while (current != NULL)
+	{
+		if (count == idx)
+		{
+			new_node->prev = current->prev;
+			new_node->next = current;
+			current->prev->next = new_node;
+			current->prev = new_node;
+			return (new_node);
+		}
+		current = current->next;
+		count++;
+	}
+	free(new_node);
+	return (NULL);
+}
