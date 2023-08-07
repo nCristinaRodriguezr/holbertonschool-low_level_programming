2af0fe891dcae9086dab03f16b8e7ec6e6680aeb
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-06 17:01:42 -0700

    task 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Funcion que devuelve el numero de nodos de una
+ * lista de doble vinculacion
+ * @head: A la lista de nodos
+ * @index: indice
+ * Return: numero de nodos
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+{
+	dlistint_t *current = head;
+	unsigned int count = 0;
+
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+	if (count == index)
+	{
+		return (current);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
