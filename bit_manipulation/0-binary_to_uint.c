9e0355795cbf1265432e74071e35fa0687b40828
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-10 18:59:00 -0700

    task 0 bit

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * binary_to_uint - Funcion que pasa un numero binario a decimal
+ * @b: puntero que apunta a una cadena de nuemeros binarios
+ * Return: Devuelve el resultado en decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int bit;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+		{
+			bit = 0;
+		}
+		else if (b[i] == '1')
+		{
+			bit = 1;
+		}
+		else
+		{
+			return (0);
+		}
+		result = (result << 1) | bit;
+	}
+	return (result);
+}
+
---
+++ bit_manipulation/main.h
@@ -0,0 +1,4 @@
+#ifndef LIST_H
+#define LIST_H
+unsigned int binary_to_uint(const char *b);
+#endif
