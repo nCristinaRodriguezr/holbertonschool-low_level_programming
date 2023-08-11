14a2579ee97e783050c614dba3991eb69b55b9be
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-11 16:35:34 -0700

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - Funcion que pasa un decimal a numero binario
+ * @n: puntero que apunta a una cadena de numero
+ */
+void print_binary(unsigned long int n);
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,4 +1,5 @@
 #ifndef LIST_H
 #define LIST_H
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
